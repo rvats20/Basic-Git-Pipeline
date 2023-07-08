55f0fa74743a6a551c906ef6d9a2f0dfd28ba522
Rahul vats <43817170+rvats20@users.noreply.github.com>
2023-07-08 11:46:44 +0530

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+def extract(file_path: str) -> DataFrame:
+    '''
+    extracts csv data and converts to pandas Dataframe
+
+    args:
+        file_path (str): path to the csv file
+    
+    returns:
+        df (DataFrame): pandas dataframe containing the csv data
+    '''
+
+
+    # TODO - # exracts the csv data as pandas dataframe
+    df = pd.read_csv('/home/rahul/Downloads/SPX.csv')
+
+    return  df
+
+
+def transform(df: DataFrame) -> DataFrame:
+    '''
+    cleans data
+
+    args:
+        df (DataFrame): pandas dataframe containing the raw data
+    
+    returns:
+        df (DataFrame): pandas dataframe containing the clean data
+    '''
+
+    # TODO - # drop null values
+    df.dropna(inplace=True)
+
+    # TODO - # remove decimal from year column and convert to string
+    
+    return df
+
+def load(df: DataFrame, save_path: str):
+    '''
+    writes pandas Dataframe to csv file
+
+    args:
+        df (DataFrame): pandas dataframe containing the clean data
+        save_path (str): path to save the csv file
+    
+    returns:
+        None
+    '''
+
+    # TODO - # write dataframe to csv
+
+    return
---
+++ pipeline.py
@@ -0,0 +1,24 @@
+from etl import *
+
+
+
+file_path = "~/basic-etl-pipeline/data/economic-indicators.csv"
+save_path = "~/basic-etl-pipeline/data/clean_economic-indicators.csv"
+
+def run_pipeline(file_path:str, save_path:str):
+
+    # TODO - extract
+    df = 
+
+    # TODO - transform
+    df = 
+
+    # TODO - load
+
+    return
+
+print(__name__)
+
+if __name__ == "__main__":
+
+    run_pipeline(file_path=file_path, save_path=save_path)
